3a1a1c05d21f25fd216694bda3c95bb2b8408d21
Danial992 <150413013+Danial992@users.noreply.github.com>
2025-08-07 16:06:13 +0500

    Update ProgramSection.tsx

--- src/components/ProgramSection.tsx
+++ src/components/ProgramSection.tsx
@@ -99,13 +99,12 @@ const ProgramSection: React.FC = () => {
             </div>
           ))}
         </div>
-      <a href="#contact-form">
-        <button className="bg-teal-600 hover:bg-teal-700 text-white font-bold py-4 px-8 rounded-lg text-xl transition-all transform hover:scale-105">
+        
+      <div className="text-center mt-12">
+          <button className="bg-teal-600 hover:bg-teal-700 text-white font-bold py-4 px-8 rounded-lg text-xl transition-all transform hover:scale-105">
             Зарегистрироваться на встречу
           </button>
-      </a>
         </div>
-
       </div>
     </section>
   );
