c84b0725aed0ac2413162dac493b9e610a4ac55b
Danial992 <150413013+Danial992@users.noreply.github.com>
2025-08-07 13:06:56 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkTeal: '#143A41',      // Темно-бирюзовый
+        mediumTeal: '#38747E',    // Средне-бирюзовый
+        lightTeal: '#53A4B1',     // Светло-бирюзовый
+        mutedTeal: '#306770',     // Приглушенно-бирюзовый
+      },
+    },
   },
   plugins: [],
 };
