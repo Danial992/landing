ac75a8299971c268137a4787719dfb67101c95be
Danial992 <150413013+Danial992@users.noreply.github.com>
2025-08-07 14:21:03 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ export default {
         mediumTeal: '#38747E',    // Средне-бирюзовый
         lightTeal: '#53A4B1',     // Светло-бирюзовый
         mutedTeal: '#306770',     // Приглушенно-бирюзовый
+
+        // Алиасы
+        primary: '#143A41',       // darkTeal
+        'primary-dark': '#38747E',// mediumTeal
+        accent: '#53A4B1',        // lightTeal
+        muted: '#306770',         // mutedTeal
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        arial: ['Arial', 'sans-serif'],
       },
     },
   },
